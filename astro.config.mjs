98453a540a932991b3b613e65ff06f952b5b0782
Manuel Arroyo Juan <109987429+Majprofe@users.noreply.github.com>
2025-05-22 16:54:33 +0200

    Update astro.config.mjs
    modificado astro.config para añadir configuración estática

--- astro.config.mjs
+++ astro.config.mjs
@@ -1,10 +1,11 @@
 // @ts-check
 import { defineConfig } from 'astro/config';
-
 import tailwindcss from '@tailwindcss/vite';
+import staticAdapter from '@astrojs/adapter-static';
 
-// https://astro.build/config
 export default defineConfig({
+  adapter: staticAdapter(),
+  output: 'static',
   vite: {
     plugins: [tailwindcss()]
   }
